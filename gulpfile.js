48a4ce918e35ce7c6039398ff0a36649c0e144ab
Xavier Araque <xavieraraque@gmail.com>
2017-03-21 08:42:55 -0400

    New version 1.0.1

--- gulpfile.js
+++ gulpfile.js
@@ -3,65 +3,47 @@ livereload      = require('gulp-livereload'),
 uglify          = require('gulp-uglifyjs'),
 sass            = require('gulp-sass'),
 autoprefixer    = require('gulp-autoprefixer'),
-sourcemaps      = require('gulp-sourcemaps'),
-image           = require('gulp-imagemin'),
-pngquant        = require('imagemin-pngquant'),
-esformatter     = require('gulp-esformatter');
+sourcemaps      = require('gulp-sourcemaps');
 
 var theme_name  = "starter-kit-wordpress" 
-var path_themes = "wp-content/themes/";
+var path_themes = "./wp-content/themes/";
 var path_theme  = path_themes + theme_name
 
-// optimized images, use $gulp image
-// gulp.task('image', function () {
-//     return gulp.src(path_theme + '/*')
-//     .pipe(image({
-//         progressive: true,
-//         svgoPlugins: [{removeViewBox: false}],
-//         use: [pngquant()]
-//     }))
-//     .pipe(gulp.dest(path_theme + '/images'));
-// });
-
-// compile sass files, use $gulp sass
 gulp.task('sass', function () {
-  gulp.src(path_theme + '/sass/**/*.scss')
+  gulp.src( path_theme + '/sass/**/*.scss')
   .pipe(sourcemaps.init())
-    // Type: String Default: nested Values: nested, expanded, compact, compressed
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(autoprefixer(
-    {
+        // Type: String Default: nested Values: nested, expanded, compact, compressed
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         // https://github.com/postcss/autoprefixer#options
-        browsers: ['last 2 versions'],
-        cascade: false
-    }
-    ))
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest(path_theme));
-});
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest( path_theme + '/css'));
+      });
 
 // compress js files, use $gulp uglify
-gulp.task('uglify', function() {
-  var path = path_theme + "/lib/bootstrap-sass-3.3.7/assets/javascripts/bootstrap/";
+gulp.task('compress', function() {
+  var ruta = "./node_modules/bootstrap-sass/assets/javascripts/bootstrap/";
   gulp.src([ 
     // Bootstrap components
-    //path + 'affix.js',
-    //path + 'alert.js',
-    //path + 'button.js',
-    path + 'carousel.js',
-    path + 'collapse.js',
-    //path + 'dropdown.js',
-    //path + 'popover.js',
-    //path + 'scrollspy.js',
-    //path + 'tab.js',
-    //path + 'tooltip.js',
-    path + 'transition.js',
+    ruta + 'affix.js',
+    ruta + 'alert.js',
+    ruta + 'button.js',
+    ruta + 'carousel.js',
+    ruta + 'collapse.js',
+    ruta + 'dropdown.js',
+    ruta + 'scrollspy.js',
+    ruta + 'tooltip.js',
+    ruta + 'popover.js',
+    ruta + 'tab.js',
+    ruta + 'tooltip.js',
+    ruta + 'transition.js',
+    ruta + 'modal.js',
     // Libraries custom theme
-    path_theme + '/lib/*.js'
+    path_theme + '/js-source/*.js',
+    //rutaflow + 'index.js'
     ])
   // Compress version
   .pipe(uglify('main.min.js'))
-
   //Uncompress version
   // .pipe(uglify('main.js', {
   //     mangle: false,
@@ -69,24 +51,14 @@ gulp.task('uglify', function() {
   //       beautify: true
   //     }
   //   }))
-
-  .pipe(gulp.dest(path_theme + '/js'))
+.pipe(gulp.dest(path_theme + '/js'))
 });
 
-// Formater JS
-// gulp.task('esformatter', function () {
-//     return gulp.src(path_theme + '/js-raw/main.js')
-//         .pipe(esformatter({indent: {value: '  '}}))
-//         .pipe(gulp.dest(path_theme + '/js-raw/output'));
-// });
-
-// Task watch, use $gulp watch
 gulp.task('watch', function(){
-    livereload.listen();
-    gulp.watch(path_theme + '/**/*.scss', ['sass']);
-    //gulp.watch('themes/custom/fisherprice/images/**/*'+'[.jpg, .JPG, .png, .PNG]', ['image']);
-    gulp.watch(path_theme + '/lib/*.js', ['uglify']);
-    gulp.watch([path_theme + '/style.css', path_theme + '/**/*.php', path_theme + '/js/*.js'], function (files){
-        livereload.changed(files)
-    });
+  livereload.listen();
+  gulp.watch( path_theme + '/sass/**/*.scss', ['sass']);
+  gulp.watch( path_theme + '/js-source/*.js', ['compress']);
+  gulp.watch([ path_theme + '/css/style.css',  path_theme + '/**/*.twig',  path_theme + '/js/*.js'], function (files){
+    livereload.changed(files)
+  });
 });
